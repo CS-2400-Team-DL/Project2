434c8b4fd7ba706dc82592a68fc9e40023d38a83
Leonardo <leonardodavalos3@gmail.com>
2022-03-07 16:00:31 -0800

    excel file created

--- src/main/LinkedStackTest.java
+++ src/main/LinkedStackTest.java
@@ -10,6 +10,8 @@ public class LinkedStackTest{
 		System.out.println(LinkedStack.convertToPostFix(Infix1));
 		String Infix2 = "2^(2*7)*(15+4)";
 		System.out.println(LinkedStack.convertToPostFix(Infix2));
+		String Infix3 = "1 * 2 / (3 - 1) + 4 * 5";
+		System.out.println(LinkedStack.convertToPostFix(Infix3));
 		
 		
 		
