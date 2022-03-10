f061ef002da73836d7dc1bc558dbad9fe00b83fc
Leonardo <leonardodavalos3@gmail.com>
2022-03-09 17:14:28 -0800

    Refined Code

--- src/main/LinkedStack.java
+++ src/main/LinkedStack.java
@@ -3,11 +3,10 @@ package main;
 import java.util.EmptyStackException;
 
 /**
- * Stack implenmation using linked data. A stack is a data struture that has a restriction where
+ * Stack implementation using linked data. A stack is a data structure that has a restriction where
  * only the top entity can be altered. 
  * Last In, First Out. 
  * First In, Last Out.
- * 
  * 
  * @param <T> Data type of stack contents.
  */
@@ -15,7 +14,7 @@ public class LinkedStack<T> implements StackInterface<T> {
 	private Node<T> nodeReference;
 
 	/** 
-	 * Default constructor for a linked stack.
+	 * Default constructor
 	 */
 	LinkedStack(){
 		nodeReference = null;
@@ -23,7 +22,6 @@ public class LinkedStack<T> implements StackInterface<T> {
 
 	@Override
 	public void push(T newEntry) {
-
 		Node<T> newNode = new Node<T>(newEntry);
 		newNode.setNextNode(nodeReference);
 		nodeReference = newNode;
@@ -38,14 +36,12 @@ public class LinkedStack<T> implements StackInterface<T> {
 		return topNode.getData();
 	}
 
-
 	@Override
 	public T peek() {
 		if (isEmpty()) { throw new EmptyStackException(); }
 		Node<T> topNode =  nodeReference;
 		return topNode.getData();
 	}
-
 
 	@Override
 	public boolean isEmpty() {
@@ -56,14 +52,15 @@ public class LinkedStack<T> implements StackInterface<T> {
 	public void clear() {
 		nodeReference = null;
 	}
+	
 	/**
-	 * @author Leonardo.
 	 * Algorithm to transform a legal infix notation equation into a postfix equation.
 	 * Will ignore any non-math symbols including white space and parse through the given equation.
-	 * Unbalanced infix notation will get an output. It will treat everything after an open paranthesis 
+	 * Unbalanced infix notation will get an {@link IllegalArgumentException}
 	 * @param inFix input to be transformed into postfix - should be a balanced equation.
 	 * @throws IllegalArgumentException if the infix input does not have all parenthesis closed
 	 * @return A string transformation of the infix input into postfix notation.
+	 * @author Leonardo
 	 */
 	public static String convertToPostFix(String inFix) {
 		
@@ -77,80 +74,73 @@ public class LinkedStack<T> implements StackInterface<T> {
 			
 			switch (activeChar) {
 			
-			case ')':
-				topOp = opStack.pop();
-				while(topOp != '(') {
-					postFix = postFix.concat(topOp.toString() + " ");
+				case ')':
 					topOp = opStack.pop();
-				}
-				break;
-				
-			case '(':
-				opStack.push(activeChar);
-				break;
-				
-			case '^':
-				opStack.push(activeChar);
-				break;
-				
-			case '*':	
-			case '/':	
-			case '+':				
-			case '-':
-				
-				while (!opStack.isEmpty() && ( LinkedStack.precedenceIndex(activeChar) <= LinkedStack.precedenceIndex(opStack.peek()))) {
+					while(topOp != '(') {
+						postFix = postFix.concat(topOp.toString() + " ");
+						topOp = opStack.pop();
+					}
+					break;
 					
-					postFix = postFix.concat(opStack.pop().toString() + " ");
-				}
-				opStack.push(activeChar);
-				break;
+				case '(':
+					opStack.push(activeChar);
+					break;
+					
+				case '^':
+					opStack.push(activeChar);
+					break;
+					
+				case '*': case '/': case '+': case '-': // All have same instructions to execute
+					
+					while (!opStack.isEmpty() && (LinkedStack.precedenceIndex(activeChar) <= LinkedStack.precedenceIndex(opStack.peek()))) {
+						postFix = postFix.concat(opStack.pop().toString() + " ");
+					}
+					opStack.push(activeChar);
+					break;
 				
-			default: // default case handles separating numbers from unexpected input. 
-				if (-1 != exceptions.indexOf(activeChar)){ // adds anything to postFix that is part of the exceptions String.
-					postFix = postFix.concat(activeChar.toString() + " ");
-				}
+				default: // default case handles separating unexpected input. 
+					if (-1 != exceptions.indexOf(activeChar)){ // adds anything to postFix that is part of the exceptions String.
+						postFix = postFix.concat(activeChar.toString() + " ");
+					}
+					break;
 				
-				break;
 			} // Switch End
-			
 		} // End of For Loop
 		
-		while (!opStack.isEmpty()) {
+		while (!opStack.isEmpty()) { // empty the opStack top to bottom
 			topOp = opStack.pop();
 			if (topOp == '(') { throw new IllegalArgumentException("Open Parenthesis");}
 			postFix = postFix.concat(topOp.toString() + " ");
 		}
 		
 		return postFix;
-		
 	} // end of Algorithm
 	
 	/**
-	 * Index of precedence level made from a switch case.
+	 * Index of precedence level made from a switch case. 
 	 * @param Must either be '^, *, /, +, -, or )' to have a proper precedence value
 	 * @return the level of precedence from a given legal math operator
+	 * @throw {@link IllegalArgumentException} when an operator not accounted for is encountered.
 	 */
 	private static int precedenceIndex(char activeOp) {
 		switch (activeOp) {
-		case '^':
-			return 5; // Highest Precedent
-		case '*':
-			return 3; // Second Highest
-		case '/':
-			return 3; // Equal to Second
-		case '+':
-			return 1; // Low Precendent
-		case '-':
-			return 1; // Same as above
-		case'(':
-			return 0; // Lowest precendent
-		default:
-			throw new IllegalArgumentException("Unknow Operator"); 
+			case '^':
+				return 5; // Highest Precedent
+			case '*':
+				return 3; // Second Highest
+			case '/':
+				return 3; // Equal to Second
+			case '+':
+				return 1; // Low Precedent
+			case '-':
+				return 1; // Same as above
+			case'(':
+				return 0; // Lowest precedent
+			default:
+				throw new IllegalArgumentException("Unknow Operator"); 
 		}
 	} // End of precedenceIndex
-
 }
-
 
 final class Node<T>{
 	private T data;
@@ -175,5 +165,4 @@ final class Node<T>{
 	public void setNextNode(Node<T> newNode){
 		this.nextNode = newNode;
 	}
-	
 }
